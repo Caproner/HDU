9d20b11f9b47c7c4b3bfe51b490a0f8e6aac6240
Caproner <634768901@qq.com>
2018-04-18 10:56:46 +0800

    增加部分缩进

--- 1000-1099/1004.cpp
+++ 1000-1099/1004.cpp
@@ -12,33 +12,33 @@ int main()
     int n,p=0;
     while(scanf("%d",&n)==1&&n)
     {
-    for(int i=0;i<1000;i++)a[i].n=0;
-    while(n--)
-    {
-        string c;
-        cin>>c;
-        int ok=1;
-        for(int i=0;i<p;i++)
+        for(int i=0;i<1000;i++)a[i].n=0;
+        while(n--)
         {
-            if(c==a[i].a)
+            string c;
+            cin>>c;
+            int ok=1;
+            for(int i=0;i<p;i++)
             {
-                a[i].n++;
-                ok=0;
-                break;
+                if(c==a[i].a)
+                {
+                    a[i].n++;
+                    ok=0;
+                    break;
+                }
+            }
+            if(ok)
+            {
+                a[p].a=c;
+                a[p].n=1;
+                p++;
             }
         }
-        if(ok)
+        int k=0;
+        for(int i=0;i<p;i++)
         {
-            a[p].a=c;
-            a[p].n=1;
-            p++;
+            if(a[k].n<a[i].n)k=i;
         }
-    }
-    int k=0;
-    for(int i=0;i<p;i++)
-    {
-        if(a[k].n<a[i].n)k=i;
-    }
-    cout<<a[k].a<<endl;
+        cout<<a[k].a<<endl;
     }
 }
