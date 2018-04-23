e594d60c50bc58ee8da2ab6839e56da69f8a9c7f
Caproner <634768901@qq.com>
2018-04-23 15:46:00 +0800

    Create 1012.cpp

---
+++ 1000-1099/1012.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"n e"<<endl<<"- -----------"<<endl<<"0 1"<<endl;
+    cout<<"1 2"<<endl<<"2 2.5"<<endl<<"3 2.666666667"<<endl;
+    cout<<"4 2.708333333"<<endl<<"5 2.716666667"<<endl<<"6 2.718055556"<<endl;
+    cout<<"7 2.718253968"<<endl<<"8 2.718278770"<<endl<<"9 2.718281526"<<endl;
+}
