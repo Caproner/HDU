39277369e22dbf9ebf55d160df0e764cc199ea98
Caproner <634768901@qq.com>
2018-05-18 22:36:06 +0800

    Create 1094.cpp

---
+++ 1000-1099/1094.cpp
@@ -0,0 +1,16 @@
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int n;
+    while(scanf("%d",&n)==1)
+    {
+        long long a,ans=0;
+        for(int i=0;i<n;i++)
+        {
+            scanf("%lld",&a);
+            ans+=a;
+        }
+        printf("%lld\n",ans);
+    }
+}
