95acc439f18b9bce3a31e69ba53fc8141f709b30
Caproner <634768901@qq.com>
2018-05-29 16:15:25 +0800

    Update 2078.cpp

--- 2000-2099/2078.cpp
+++ 2000-2099/2078.cpp
@@ -1,40 +1,28 @@
-#include<stdio.h>
-int a[40];
-void qs(int l,int r)
-{
-    int i=l,j=r,p=i,t;
-    while(i<j)
-    {
-        for(;a[j]>=a[p]&&j>p;j--);
-        t=a[p];
-        a[p]=a[j];
-        a[j]=t;
-        p=j;
-        for(;a[i]<=a[p]&&i<p;i++);
-        t=a[p];
-        a[p]=a[i];
-        a[i]=t;
-        p=i;
-    }
-    if(l<r)
-    {
-        qs(l,p-1);
-        qs(p+1,r);
-    }
-}
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+
 int main()
 {
-    //freopen("data.in","r",stdin);
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {
-        int n,m;
-        scanf("%d %d",&n,&m);
-        for(int i=0;i<n;i++)scanf("%d",&a[i]);
-        qs(0,n-1);
-        int ans=100-a[0];
-        ans=ans*ans;
-        printf("%d\n",ans);
-    }
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		int n,m;
+		scanf("%d%d",&n,&m);
+		int ans;
+		scanf("%d",&ans);
+		for(int i=1;i<n;i++)
+		{
+			int p;
+			scanf("%d",&p);
+			ans=min(ans,p);
+		}
+		ans=100-ans;
+		ans=ans*ans;
+		printf("%d\n",ans);
+	}
+	return 0;
 }
